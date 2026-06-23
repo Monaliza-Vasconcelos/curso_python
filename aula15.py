c1eb026ee6813135eb63e63755244d6791fb6380
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2026-06-23 20:06:04 -0300

    aula15

---
+++ aula15.py
@@ -0,0 +1,9 @@
+#função input para coleta de dados
+#print(f'O seu {nome=}') para aparecer a palavra nome
+
+
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input('Digite outro número: '))
+
+
+print(f'A soma dos números é: {numero_1 + numero_2}')
