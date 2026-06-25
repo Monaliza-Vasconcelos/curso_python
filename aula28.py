111d45f94354c8e1bd96214e2d253ddb3a13258f
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2026-06-25 20:06:49 -0300

    aula28.py

---
+++ aula28.py
@@ -0,0 +1,18 @@
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+nome_invertido = nome[::-1]
+nome_ajustado = nome.strip()
+n = len(nome_ajustado)
+if nome and idade:
+    print(f'Seu nome é {nome}')
+    print(f'Seu nome invertido é {nome_invertido}')
+    if "" in nome:
+        print(f'Seu {nome} contém espaços')
+    else:
+        print(f'Seu {nome} não contém espaços')
+    print(f'Seu nome tem {n} letras')
+    print(f'A primeira letra do seu nome é {nome[0]}')
+    print(f'A última letra do seu nome é {nome[-1]}')
+else:
+    print('Desculpe, você deixou campos vazios.')
+
