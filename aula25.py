e696067daa6c6c32026ff730ad974a4a03f394d2
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2026-06-25 18:37:55 -0300

    aula25

---
+++ aula25.py
@@ -0,0 +1,13 @@
+#interpolação
+"""
+    s - string
+    d e i - int
+    f - float
+    x e X - hexadecimal
+"""
+
+
+nome = "Monaliza"
+preco = 10000.0123123
+variavel = '%s, o preço é R$%.2f' % (nome, preco)
+print(variavel)
