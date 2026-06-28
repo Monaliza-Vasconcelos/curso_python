1d156ad1453e556506438cd633b67a09d6d91643
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2026-06-28 19:45:37 -0300

    aula34

---
+++ aula34.py
@@ -0,0 +1,15 @@
+"""
+    Repetições
+    while(enquanto)
+    Executa uma ação enquanto uma condição for verdadeira
+"""
+
+
+
+while True:
+   nome = input('Qual o seu nome? ')
+
+   if nome  == "sair":
+      break
+   
+print('Acabou')
