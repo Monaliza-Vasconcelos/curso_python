9d11c316913314cdce916931868a4380b4926ba6
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2026-06-28 20:12:17 -0300

    aula35

---
+++ aula35.py
@@ -0,0 +1,14 @@
+"""
+    Repetições
+    while(enquanto)
+    Executa uma ação enquanto uma condição for verdadeira
+"""
+
+contador = 0
+
+
+while contador < 10:
+    contador += 1
+    print(contador)
+
+print('Acabou')
