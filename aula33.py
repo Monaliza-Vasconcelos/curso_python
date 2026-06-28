270fff5c1029daa4c9288913b9c839421802a018
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2026-06-28 19:38:23 -0300

    aula33

---
+++ aula33.py
@@ -0,0 +1,11 @@
+"""
+    imutáveis que vimos: str, int, float, bool
+"""
+
+string = 'monaliza Vasconcelos'
+# outra_variável = f'{string[:3]}ABC'
+
+# print(outra_variável)
+
+print(string.capitalize())
+print(string.zfill(100))
