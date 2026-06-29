c69456c211120e6965a799f1c020c32d853bfa2e
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2026-06-28 21:29:17 -0300

    aaula41

---
+++ aula41.py
@@ -0,0 +1,16 @@
+"""
+    while/else
+"""
+
+string = 'valor qualquer'
+
+i =  0
+while i < len(string):
+    letra = string[i]
+
+
+    print(letra)
+    i += 1
+
+else:
+    print('O else foi executado.')
